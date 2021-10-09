dd0a0b4af7e124dcf72cb4840d1b8faaab1ade1d
Đỗ Thị Thanh Thi <thidttpy@gmail.com>
2021-10-09 16:40:11 +0700

    Initial commit

---
+++ app/src/main/java/com/thanhthido/myapplication/example.kt
@@ -0,0 +1,28 @@
+package com.thanhthido.myapplication
+
+fun main() {
+    // + - * /
+    // cong chuoi, cong gia tri int, cong gia tri foat (+ - * /)
+    // khai bao tat ca cac kieu gia tri
+    // khai bao dung cach dat ten
+    val stringA = "Hello"
+    val stringB = "World"
+    println("Add String: $stringA $stringB")
+
+    val intA = 10
+    val intB = 5
+    println("Add Int: ${intA + intB}")
+    println("Sub Int: ${intA - intB}")
+    println("Mul Int: ${intA * intB}")
+    println("Div Int: ${intA / intB}")
+
+    val floatA = 20.5
+    val floatB = 8.2
+    println("Add float: ${floatA + floatB}")
+    println("Sub float: ${floatA - floatB}")
+    println("Mul float: ${floatA * floatB}")
+    println("Div float: ${floatA / floatB}")
+}
+
+
+
